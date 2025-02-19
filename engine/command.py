af80775c1f3be3d8946ad18a8b55dc91a2ab7f1d
Jbftech <jbftech.pro@gmail.com>
2025-02-19 23:56:57 +0400

    day3

---
+++ engine/command.py
@@ -0,0 +1,34 @@
+import pyttsx3
+import speech_recognition as sr
+
+def speak(text):
+  engine = pyttsx3.init('sapi5')
+  voices = engine.getProperty('voices')
+  engine.setProperty('voice', voices[0].id)
+  engine.setProperty('rate', 174)
+  print(voices)
+  engine.say(text)
+  engine.runAndWait()
+
+def takecommand():
+  
+  r = sr.Recognizer()
+
+  with sr.Microphone() as source:
+    print('écoute...')
+    r.pause_threshold = 1
+    r.adjust_for_ambient_noise(source)
+
+    audio = r.listen(source, 10, 6)
+
+    try:
+      print('reconnaissance')
+      query = r.recognize_assemblyai(audio, language='fr-FR') 
+      print(f"l'utilisateur a dit:{query}")
+    except Exception as e:
+           return ""
+      
+    return query.lower()
+    
+text = takecommand()
+
